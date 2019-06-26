a756bb760b16128fe93af837f49dd232eba84785
Terje Uglebakken <teug91@gmail.com>
2019-06-26 21:02:00 +0200

    chore: updated sql views

--- entry/sqlViews/DEO.sql
+++ entry/sqlViews/DEO.sql
@@ -72,9 +72,14 @@ INNER JOIN dataelement d ON v.dataelementid = d.dataelementid AND d.uid = 'lIkk6
 LEFT JOIN (trackedentitydatavalue v2
 INNER JOIN dataelement d2 ON v2.dataelementid = d2.dataelementid AND d2.uid = 'SaQe2REkGVw'
 INNER JOIN optionvalue ov ON d2.optionsetid = ov.optionsetid AND v2.value = ov.code) ON e.programstageinstanceid = v2.programstageinstanceid
+LEFT JOIN (trackedentitydatavalue v3
+INNER JOIN dataelement d3 ON v3.dataelementid = d3.dataelementid AND d3.uid = 'tAyVrNUTVHX') ON e.programstageinstanceid = v3.programstageinstanceid
+LEFT JOIN (trackedentitydatavalue v4
+INNER JOIN dataelement d4 ON v4.dataelementid = d4.dataelementid AND d4.uid = 'sXDQT6Yaf77') ON e.programstageinstanceid = v4.programstageinstanceid
 WHERE v.storedby = '${username}'
 AND o.path LIKE '%${orgunit}%'
 AND e.status = 'ACTIVE'
+AND ((v3.value IS NULL OR v3.value = 'Resend') AND (v4.value IS NULL OR v4.value = 'Resend'))
 ORDER BY e.lastupdated DESC
 
 -- DEO incomplete count:
@@ -84,9 +89,14 @@ INNER JOIN program pr ON pi.programid = pr.programid
 INNER JOIN organisationunit o ON e.organisationunitid = o.organisationunitid
 INNER JOIN (trackedentitydatavalue v
 INNER JOIN dataelement d ON v.dataelementid = d.dataelementid AND d.uid = 'lIkk661BLpG') ON e.programstageinstanceid = v.programstageinstanceid
+LEFT JOIN (trackedentitydatavalue v3
+INNER JOIN dataelement d3 ON v3.dataelementid = d3.dataelementid AND d3.uid = 'tAyVrNUTVHX') ON e.programstageinstanceid = v3.programstageinstanceid
+LEFT JOIN (trackedentitydatavalue v4
+INNER JOIN dataelement d4 ON v4.dataelementid = d4.dataelementid AND d4.uid = 'sXDQT6Yaf77') ON e.programstageinstanceid = v4.programstageinstanceid
 WHERE v.storedby = '${username}'
 AND o.path LIKE '%${orgunit}%'
 AND e.status = 'ACTIVE'
+AND ((v3.value IS NULL OR v3.value = 'Resend') AND (v4.value IS NULL OR v4.value = 'Resend'))
 
 -- DEO rejected:
 SELECT v.value AS amrid, pr.name AS organismgroup, ov.name AS organism, e.created::date, e.lastupdated::date, o.uid AS ouid, e.uid AS eventid FROM programstageinstance e
