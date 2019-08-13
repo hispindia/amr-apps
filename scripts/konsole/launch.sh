6971900b033bd6c3bc5d26ce78865d67b9861224
Terje Uglebakken <teug91@gmail.com>
2019-08-13 14:00:40 +0200

    chore: add base monorepo architecture

--- scripts/konsole/launch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-konsole --tabs-from-file /home/terje/Documents/uio/master/amr-apps/scripts/konsole/tabs
--- src/actions/index.js
+++
@@ -1,4 +0,0 @@
-export * from './alert'
-export * from './data'
-export * from './metadata'
-export * from './orgUnit'
