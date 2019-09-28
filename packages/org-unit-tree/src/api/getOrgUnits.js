134f00dcb05f233f6134abd9a19d6440fb77799b
Terje Uglebakken <teug91@gmail.com>
2019-09-28 23:56:21 +0200

    fix(org-unit-tree): fix only getting user org units

--- packages/org-unit-tree/src/api/getOrgUnits.js
+++ packages/org-unit-tree/src/api/getOrgUnits.js
@@ -1,10 +1,10 @@
 import { get, request } from '@hisp-amr/api'
 
-export const getOrgUnits = async ({ root }) =>
+export const getOrgUnits = async root =>
     await get(
         request('organisationUnits', {
             order: 'level:asc',
             fields: ['children', 'displayName', 'id', 'path, code'],
-            filter: `path:like:${root}`,
+            filters: `path:ilike:${root}`,
         })
     )
